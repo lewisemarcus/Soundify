204bbc7f2cea9d795650de8c112d206061bf3670
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-05-05 22:25:57 -0700

    testing with separate folders, moving files

--- server/server.js
+++ server/server.js
@@ -23,7 +23,9 @@ app.use("/", deleteMovie)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"))
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+        res.sendFile(
+            path.join(__dirname, "..", "client", "build", "index.html"),
+        )
     })
 }
 
