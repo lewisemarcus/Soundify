e071571d95b05e24e4bc9d356a8f0a67a7092293
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-05-07 21:34:53 -0700

    added user schema, updated songschema

--- server/models/songSchema.js
+++ server/models/songSchema.js
@@ -1,12 +1,52 @@
 const mongoose = require("mongoose")
 
-const songSchema = {
-    title: String,
-    genre: String,
-    year: String,
-    filename: String,
-    link: String,
-}
+// const songSchema = {
+//     title: String,
+//     genre: String,
+//     year: String,
+//     filename: String,
+//     link: String,
+// }
+
+const songSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        genre: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        year: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        filename: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        link: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    },
+)
 
 const Song = mongoose.model("Song", songSchema)
 
