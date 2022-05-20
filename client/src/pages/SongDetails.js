adf20b490cf813bc101729472351f59f3236953f
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-05-19 19:37:44 -0700

    init setup for song details page

---
+++ client/src/pages/SongDetails.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react"
+import { useParams } from "react-router-dom"
+
+const SongDetails = () => {
+    const { songId } = useParams()
+    return <div></div>
+}
+
+export default SongDetails
