9433fe94fc39326aa66a518d9a475b3317fb632f
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-05-19 20:57:54 -0700

    some updates to music player page

--- client/src/pages/SongDetails.js
+++ client/src/pages/SongDetails.js
@@ -1,9 +1,42 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, createRef } from "react"
+import SongDetailsPlayer from "../components/SongDetailsPlayer"
 import { useParams } from "react-router-dom"
+
+const containerStyle = {
+    marginBottom: 20,
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    alignItems: "center",
+}
 
 const SongDetails = () => {
     const { songId } = useParams()
-    return <div></div>
+    const app = createRef()
+
+    useEffect(() => {
+        const sketch = SongDetailsPlayer(
+            "https://soundclone-music.s3.amazonaws.com/qwe",
+            app,
+        )
+    })
+
+    return (
+        <div>
+            <button
+                onClick={() => {
+                    SongDetailsPlayer(
+                        "https://soundclone-music.s3.amazonaws.com/qwe",
+                        app,
+                    )
+                }}
+            >
+                Play
+            </button>
+            <div ref={app} id="canvasContainer" style={containerStyle}></div>
+        </div>
+    )
 }
 
 export default SongDetails
