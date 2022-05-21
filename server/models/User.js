cbea79ea6f7f328c79c9101d32f69c07aa55fd9d
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-05-21 13:55:51 -0700

    updated typdefs and models

--- server/models/User.js
+++ server/models/User.js
@@ -1,16 +1,22 @@
-import pkg from "mongoose";
-const { model, Schema } = pkg;
+import pkg from "mongoose"
+const { model, Schema } = pkg
 
 const userSchema = new Schema({
-  username: { type: String, required: true, default: null },
-  email: {
-    type: String,
-    required: true,
-    match: [/.+@.+\..+/, "Must match an email address!"],
-    unique: true,
-  },
-  password: { type: String, required: true, minlength: 5 },
-  token: { type: String },
-});
+    username: { type: String, required: true, default: null },
+    email: {
+        type: String,
+        required: true,
+        match: [/.+@.+\..+/, "Must match an email address!"],
+        unique: true,
+    },
+    password: { type: String, required: true, minlength: 5 },
+    token: { type: String },
+    songs: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Song",
+        },
+    ],
+})
 
-export default model("User", userSchema);
+export default model("User", userSchema)
