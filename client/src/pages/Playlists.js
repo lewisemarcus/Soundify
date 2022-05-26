88e30a43357560c1b93e62e27dd7bbe9293403b6
Tyler Welker <twelker08@gmail.com>
2022-05-25 21:07:53 -0400

    working on playlists page

--- client/src/pages/Playlists.js
+++ client/src/pages/Playlists.js
@@ -1,15 +1,15 @@
 import React from "react";
 import AudioPlayer from "../components/MusicPlayer/AudioPlayer";
 import tracks from "../components/MusicPlayer/tracks";
-import { AiFillCloseCircle } from "react-icons/ai";
+// import { AiFillCloseCircle } from "react-icons/ai";
 import "./styles/Playlists.css"
-import { Row, Col } from "antd"
-import PlaylistCard from "../components/Playlists"
+// import { Row, Col } from "antd"
+import PlaylistCard from "../components/PlaylistCard"
 
 const Playlists = (props) => {
 
-const renderPlayList = props.songs.map((songs) => {
-  return <PlaylistCard song={songs}></PlaylistCard>;
+const renderPlayList = props.playlist.map((playlist) => {
+  return <PlaylistCard playlist={playlist}></PlaylistCard>;
 })
 
   return (
@@ -18,17 +18,7 @@ const renderPlayList = props.songs.map((songs) => {
       <div className="Playlist-container">
           <AudioPlayer tracks={tracks} />
           <div className="item">
-              <h2 className="playlist-title">Playlist Name:</h2>
-              <div className="headers">
-                <Row>
-                  <Col span={8}><h2 className="playlist-header">Title</h2></Col>
-                  <Col span={8}><h2 className="playlist-header">Artist</h2></Col>
-                  <Col span={8}><h2 className="playlist-header">Remove</h2></Col>
-                </Row>
-            </div>
-            <div className="content">
-              {renderPlayList}
-            </div>
+            {renderPlayList}
           </div>
       </div>
     </div>
