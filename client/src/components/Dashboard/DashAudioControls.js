11442870330b0ef9b15e7264a44516c98c9f42ad
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-05-30 18:42:52 -0700

    updated pause play feature of dash

--- client/src/components/Dashboard/DashAudioControls.js
+++ client/src/components/Dashboard/DashAudioControls.js
@@ -1,52 +1,53 @@
-import React from "react";
-import { ReactComponent as Play } from "./assets/play.svg";
-import { ReactComponent as Pause } from "./assets/pause.svg";
-import { ReactComponent as Next } from "./assets/next.svg";
-import { ReactComponent as Prev } from "./assets/prev.svg";
+import React from "react"
+import { ReactComponent as Play } from "./assets/play.svg"
+import { ReactComponent as Pause } from "./assets/pause.svg"
+import { ReactComponent as Next } from "./assets/next.svg"
+import { ReactComponent as Prev } from "./assets/prev.svg"
 
 const DashAudioControls = ({
-  isPlaying,
-  onPlayPauseClick,
-  onPrevClick,
-  onNextClick
+    isPlaying,
+    onPlayPauseClick,
+    onPrevClick,
+    onNextClick,
+    genreBool,
 }) => (
-  <div className="audio-ctrls">
-    <button
-      type="button"
-      className="previous"
-      aria-label="Previous"
-      onClick={onPrevClick}
-    >
-      <Prev />
-    </button>
-    {isPlaying ? (
-      <button
-        type="button"
-        className="pause-btn"
-        onClick={() => onPlayPauseClick(false)}
-        aria-label="Pause"
-      >
-        <Pause />
-      </button>
-    ) : (
-      <button
-        type="button"
-        className="play-btn"
-        onClick={() => onPlayPauseClick(true)}
-        aria-label="Play"
-      >
-        <Play />
-      </button>
-    )}
-    <button
-      type="button"
-      className="next-btn"
-      aria-label="Next"
-      onClick={onNextClick}
-    >
-      <Next />
-    </button>
-  </div>
-);
+    <div className="audio-ctrls">
+        <button
+            type="button"
+            className="previous"
+            aria-label="Previous"
+            onClick={onPrevClick}
+        >
+            <Prev />
+        </button>
+        {isPlaying && !genreBool ? (
+            <button
+                type="button"
+                className="pause-btn"
+                onClick={() => onPlayPauseClick(false)}
+                aria-label="Pause"
+            >
+                <Pause />
+            </button>
+        ) : (
+            <button
+                type="button"
+                className="play-btn"
+                onClick={() => onPlayPauseClick(true)}
+                aria-label="Play"
+            >
+                <Play />
+            </button>
+        )}
+        <button
+            type="button"
+            className="next-btn"
+            aria-label="Next"
+            onClick={onNextClick}
+        >
+            <Next />
+        </button>
+    </div>
+)
 
-export default DashAudioControls;
+export default DashAudioControls
