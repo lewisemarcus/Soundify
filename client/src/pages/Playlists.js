bd8c5257920c776488e9320e7b6993a50e166793
Tyler Welker <twelker08@gmail.com>
2022-05-31 16:41:10 -0400

    fixing footer on playlist page

--- client/src/pages/Playlists.js
+++ client/src/pages/Playlists.js
@@ -8,33 +8,32 @@ import PlaylistCard from "../components/PlaylistCard"
 
 const Playlists = () => {
 
-// const renderPlayList = props.playlist.map((playlist) => {
-//   return <PlaylistCard playlist={playlist}></PlaylistCard>;
-// })
+  // const renderPlayList = props.playlist.map((playlist) => {
+  //   return <PlaylistCard playlist={playlist}></PlaylistCard>;
+  // })
 
   return (
-    <div>
+    <div style={{height:'100vh'}}>
       <h1>Playlist created by user</h1>
       <div className="Playlist-container">
-          <AudioPlayer tracks={tracks} />
-          <div className="item">
+        <AudioPlayer tracks={tracks} />
+        <div className="item">
           <div className="content">
             <h2 className="playlist-title">Playlist Name:</h2>
             <div className="headers">
-                <Row>
-                    <Col span={8}><h2 className="playlist-header">Title</h2></Col>
-                    <Col span={8}><h2 className="playlist-header">Artist</h2></Col>
-                    <Col span={8}><h2 className="playlist-header">Remove</h2></Col>
-                </Row>
+              <Row>
+                <Col span={8}><h2 className="playlist-header">Title</h2></Col>
+                <Col span={8}><h2 className="playlist-header">Artist</h2></Col>
+                <Col span={8}><h2 className="playlist-header">Remove</h2></Col>
+              </Row>
             </div>
             <Row>
-                <Col span={8}><h2 className="playlist-header">Test</h2></Col>
-                <Col span={8}><h2 className="playlist-header">Test</h2></Col>
-                <Col span={8}><i className="trashcan" style={{ color: 'red', marginTop: '7px' }}><AiFillCloseCircle /></i></Col>
+              <Col span={8}><h2 className="playlist-header">Test</h2></Col>
+              <Col span={8}><h2 className="playlist-header">Test</h2></Col>
+              <Col span={8}><i className="trashcan" style={{ color: 'red', marginTop: '7px' }}><AiFillCloseCircle /></i></Col>
             </Row>
-        </div>
-            {/* {renderPlayList} */}
           </div>
+        </div>
       </div>
     </div>
   );
