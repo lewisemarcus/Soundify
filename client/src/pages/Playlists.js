98adff23194e0adb888c4cb7172c757c636f2e5d
Tyler Welker <twelker08@gmail.com>
2022-06-02 18:16:38 -0400

    throwing songs on playlistpage

--- client/src/pages/Playlists.js
+++ client/src/pages/Playlists.js
@@ -17,7 +17,7 @@ const Playlists = () => {
   }
 
   return (
-    <div style={{ height: '100vh' }}>
+    <div>
       <h1>Playlist created by user</h1>
       <div className="Playlist-container">
         <AudioPlayerContainer />
@@ -31,11 +31,22 @@ const Playlists = () => {
                 <Col span={8}><h2 className="playlist-header">Remove</h2></Col>
               </Row>
             </div>
-            <Row>
-              <Col span={8}><h2 className="playlist-header" title={songs.title}></h2></Col>
-              <Col span={8}><h2 className="playlist-header" artist={songs.artist}></h2></Col>
-              <Col span={8}><i className="trashcan" style={{ color: 'red', marginTop: '7px' }}><AiFillCloseCircle /></i></Col>
-            </Row>
+            {songs.map((song, index) => {
+            return (
+              <div className="playlistCard" key={index}>
+                <Row style={{color: 'white'}}>
+                  <Col span={8}><h2 className="playlist-header">{song.title}</h2></Col>
+                  <Col span={8}><h2 className="playlist-header">{song.artist}</h2></Col>
+                  <Col span={8}>
+                    <button id="removeBtn">
+                      <i className="trashcan" style={{ color: 'red', marginTop: '7px' }}><AiFillCloseCircle /></i>
+                    </button>
+                  </Col>
+                </Row>
+              </div>
+            );
+          })}
+            
           </div>
         </div>
       </div>
