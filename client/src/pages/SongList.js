0eba2f6f413b0a81f77a4c4c037bdf4629cd2496
Calvin Kim <calvinkim1230@gmail.com>
2022-06-01 20:57:29 -0700

    updated modal input functionality

--- client/src/pages/SongList.js
+++ client/src/pages/SongList.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import { Modal, message, Upload, Input, Select, Space } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
 import Button from "../components/Button";
@@ -6,10 +6,9 @@ import "./styles/SongList.css";
 import { GET_USER_SONGS } from "../utils/queries/songQueries";
 import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
-import { AuthContext } from "../context/authContext";
+import axios from "axios";
 
 const SongList = () => {
-  const { user } = useContext(AuthContext);
   const username = localStorage.getItem("username");
   const { loading, data } = useQuery(GET_USER_SONGS, {
     variables: { username: username },
@@ -18,14 +17,16 @@ const SongList = () => {
   const [song, setSong] = useState({
     title: "",
     genre: "",
-    username: user.username,
+    username: username,
     artist: "",
     filename: "",
     link: "",
   });
+  const [file, setFile] = useState(null);
 
-  const success = () => {
-    message.success("Successfully uploaded song!");
+  const success = async () => {
+    await message.loading("Uploading song...");
+    await message.success("Successfully uploaded song!");
   };
 
   const error = () => {
@@ -59,18 +60,48 @@ const SongList = () => {
     setIsModalVisible(true);
   };
 
-  const handleOk = () => {
-    if (song.title || song.artist || song.genre || song.filename === "") {
+  const handleOk = async () => {
+    if (
+      song.title === "" ||
+      song.artist === "" ||
+      song.genre === "" ||
+      file === null
+    ) {
+      return error();
+    }
+    setIsModalVisible(false);
+    success();
+    const tags = song.title.split(" ");
+    tags.push(song.genre, song.artist.split(" "));
+    const formData = new FormData();
+    formData.append("username", song.username);
+    formData.append("genre", song.genre);
+    formData.append("artist", song.artist);
+    formData.append("title", song.title);
+    formData.append("tags", tags);
+    formData.append("filename", file);
+    try {
+      const res = await axios({
+        method: "post",
+        url: "/upload",
+        data: formData,
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      await window.location.reload();
+    } catch (err) {
       error();
-    } else {
-      success();
-      window.location.reload();
-      setIsModalVisible(false);
     }
   };
 
   const handleCancel = () => {
-    window.location.reload();
+    setSong({
+      title: "",
+      genre: "",
+      username: username,
+      artist: "",
+      filename: "",
+      link: "",
+    });
     setIsModalVisible(false);
   };
 
@@ -89,22 +120,22 @@ const SongList = () => {
     accept: "audio/*",
     multiple: false,
     // action: "http://localhost:4000/upload",
-    customRequest({ onSuccess, onError, file }) {
-      const tags = song.title.split(" ");
-      tags.push(song.genre, song.artist.split(" "));
-      const formData = new FormData();
-      formData.append("username", song.username);
-      formData.append("genre", song.genre);
-      formData.append("artist", song.artist);
-      formData.append("title", song.title);
-      formData.append("tags", tags);
-      formData.append("filename", file);
-      fetch("/upload", {
-        method: "POST",
-        body: formData,
-        mode: "cors",
-      });
-    },
+    // customRequest({ onSuccess, onError, file }) {
+    //   const tags = song.title.split(" ");
+    //   tags.push(song.genre, song.artist.split(" "));
+    //   const formData = new FormData();
+    //   formData.append("username", song.username);
+    //   formData.append("genre", song.genre);
+    //   formData.append("artist", song.artist);
+    //   formData.append("title", song.title);
+    //   formData.append("tags", tags);
+    //   formData.append("filename", file);
+    //   fetch("/upload", {
+    //     method: "POST",
+    //     body: formData,
+    //     mode: "cors",
+    //   });
+    // },
     onChange(info) {
       const { status } = info.file;
       if (status !== "uploading") {
@@ -120,11 +151,13 @@ const SongList = () => {
       console.log("Dropped files", e.dataTransfer.files);
     },
     beforeUpload(file) {
+      setFile(file);
       console.log(file.size / 1024 / 1024);
       const isLt2M = file.size / 1024 / 1024 < 100;
       if (!isLt2M) {
         message.error("Image must smaller than 2MB!");
       }
+      return false;
     },
   };
   if (loading) {
@@ -144,6 +177,7 @@ const SongList = () => {
           visible={isModalVisible}
           onOk={handleOk}
           onCancel={handleCancel}
+          destroyOnClose
         >
           <Space direction="vertical" size="middle" style={{ display: "flex" }}>
             <Input
