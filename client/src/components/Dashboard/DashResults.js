1175157704cdd66999a1061ed7e481d0debd6cee
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-06-02 19:20:52 -0700

    temporarily removed expiration check for jwt, dashresults functionality

--- client/src/components/Dashboard/DashResults.js
+++ client/src/components/Dashboard/DashResults.js
@@ -1,49 +1,43 @@
-import "./styles/DashResults.css";
-import { useEffect } from "react";
+import "./styles/DashResults.css"
+import { useEffect } from "react"
 
-const DashResults = ({ dashSearchResults, setCurrentSong, setOneSongClick }) => {
-//   useEffect(() => {
-//     console.log(dashSearchResults);
-//   });
+const DashResults = ({
+    dashSearchResults,
+    setCurrentSong,
+    setOneSongClick,
+}) => {
+    const handleSearchClick = (event) => {
+        event.preventDefault()
 
-  const handleSearchClick = (event) => {
-
-    event.preventDefault();
-    // event.currentTarget.name;
-    
-    if (setOneSongClick !== undefined) {
-        console.log("Ricky Bobby")
-        setOneSongClick(true)
-        setCurrentSong(event.currentTarget.name)
+        if (setOneSongClick !== undefined) {
+            setOneSongClick(true)
+            setCurrentSong(event.currentTarget.name)
+        }
     }
-  }
 
-
-  console.log(dashSearchResults)
-  return (
-    
-    <div className="searchResults">
-        <div className="subResults">
-          {dashSearchResults.map((song, index) => {
-            return (
-                <>
-                <button className="resultsCard" id="resultsBtn" name={song.link} key={index} onClick={handleSearchClick}>
-                <h1>Title: {song.title}</h1>
-                <p>Artist: {song.artist}</p>
-                <p>Genre: {song.genre}</p>
-                {/* <p>Link: {song.link}</p> */}
-                </button>
-                </>
-            );
-          })}
+    return (
+        <div className="searchResults">
+            <div className="subResults">
+                {dashSearchResults.map((song, index) => {
+                    return (
+                        <>
+                            <button
+                                className="resultsCard"
+                                id="resultsBtn"
+                                name={song.link}
+                                key={index}
+                                onClick={handleSearchClick}
+                            >
+                                <h1>Title: {song.title}</h1>
+                                <p>Artist: {song.artist}</p>
+                                <p>Genre: {song.genre}</p>
+                            </button>
+                        </>
+                    )
+                })}
+            </div>
         </div>
-        {/* <div className="siDetailTexts">
-            <span className="siTaxOp">Login to listen</span>
-            <button className="siCheckButton">Listen Now</button>
-          </div> */}
-      
-    </div>
-  );
-};
+    )
+}
 
-export default DashResults;
+export default DashResults
