adf4bc546644d509cfc4275f2dcd684d54268474
Calvin Kim <calvinkim1230@gmail.com>
2022-06-06 21:28:06 -0700

    updated styling of pages

--- client/src/components/Dashboard/DashResults.js
+++ client/src/components/Dashboard/DashResults.js
@@ -1,41 +1,41 @@
-import { SearchBarContext } from "../../context/searchBarContext"
-import "./styles/DashResults.css"
-import { useContext } from "react"
-import { PageHeader } from "antd"
-import DashResultCard from "./DashResultCard"
-import { useNavigate } from "react-router-dom"
+import { SearchBarContext } from "../../context/searchBarContext";
+import "./styles/DashResults.css";
+import { useContext } from "react";
+import { PageHeader } from "antd";
+import DashResultCard from "./DashResultCard";
+import { useNavigate } from "react-router-dom";
 
 const DashResults = ({
-    dashSearchResults,
-    setCurrentSong,
-    setOneSongClick,
-    currentPlayer,
-    setIsPlaying,
+  dashSearchResults,
+  setCurrentSong,
+  setOneSongClick,
+  currentPlayer,
+  setIsPlaying,
 }) => {
-    let searchResults =
-        dashSearchResults || JSON.parse(localStorage.getItem("searchResults"))
-    const { searchBar } = useContext(SearchBarContext)
-    const navigate = useNavigate()
+  let searchResults =
+    dashSearchResults || JSON.parse(localStorage.getItem("searchResults"));
+  const { searchBar } = useContext(SearchBarContext);
+  const navigate = useNavigate();
 
-    return (
-        <div className="dash-result-container">
-            <PageHeader
-                className="site-page-header"
-                onBack={() => navigate(-1)}
-                title={`Search results for "${searchBar}"`}
-            />
+  return (
+    <div className="dash-result-container">
+      <PageHeader
+        className="site-page-header"
+        onBack={() => navigate(-1)}
+        title={`Search results for "${searchBar}"`}
+      />
 
-            {searchResults.map((searchItem) => (
-                <DashResultCard
-                    setIsPlaying={setIsPlaying}
-                    setCurrentSong={setCurrentSong}
-                    dashSearchResults={searchItem}
-                    setOneSongClick={setOneSongClick}
-                    currentPlayer={currentPlayer}
-                />
-            ))}
-        </div>
-    )
-}
+      {searchResults.map((searchItem) => (
+        <DashResultCard
+          setIsPlaying={setIsPlaying}
+          setCurrentSong={setCurrentSong}
+          dashSearchResults={searchItem}
+          setOneSongClick={setOneSongClick}
+          currentPlayer={currentPlayer}
+        />
+      ))}
+    </div>
+  );
+};
 
-export default DashResults
+export default DashResults;
