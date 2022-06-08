08760219a66f533106e66f482bad5d6e0da3626d
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-06-08 16:09:13 -0700

    empty main

--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const getMovieRouter = require("./movies/getMovies")
-
-const postMovieRouter = require("./movies/addMovie")
-
-const deleteMovie = require("./movies/deleteMovie")
-
-module.exports = { getMovieRouter, postMovieRouter, deleteMovie }
