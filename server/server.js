818b71a9e457d607da91d2f64770b1edd1df36ca
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-06-08 18:45:12 -0700

    updated server

--- server/server.js
+++ server/server.js
@@ -55,10 +55,8 @@ mongoose
         )
     })
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "..", "client", "build")))
+    app.use(express.static(path.join(__dirname, "..", "public", "index.html")))
     app.get("*", (req, res) => {
-        res.sendFile(
-            path.join(__dirname, "..", "client", "build", "index.html"),
-        )
+        res.sendFile(path.join(__dirname, "..", "public", "index.html"))
     })
 }
