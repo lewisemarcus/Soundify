6c38a3b8d09d9f4f6871870d1a4d1977d4d9caa3
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-06-08 19:23:43 -0700

    test

--- server/server.js
+++ server/server.js
@@ -55,8 +55,14 @@ mongoose
         )
     })
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "..", "public", "index.html")))
+    app.use(
+        express.static(
+            path.join(__dirname, "..", "client", "public", "index.html"),
+        ),
+    )
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+        res.sendFile(
+            path.join(__dirname, "..", "client", "public", "index.html"),
+        )
     })
 }
