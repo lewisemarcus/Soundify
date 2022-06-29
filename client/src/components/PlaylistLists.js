013087ff57e31443f020078310687f370ad0b4ea
Calvin Kim <calvinkim@Calvins-Air.lan>
2022-06-28 22:57:19 -0700

    updated styles

--- client/src/components/PlaylistLists.js
+++ client/src/components/PlaylistLists.js
@@ -1,119 +1,118 @@
-import { useNavigate } from "react-router-dom"
-import { useEffect } from "react"
-import orange from "../assets/orange.png"
-import shakeygraves from "../assets/shakeygraves.jpg"
-import "./styles/PlaylistList.css"
-import "./Dashboard/styles/Dashboard2.scss"
-import playBtn from "../assets/playBtn.png"
-import pauseBtn from "../assets/pauseBtn.png"
-import { DeleteOutlined } from "@ant-design/icons"
-import { useMutation } from "@apollo/client"
-import { REMOVE_FROM_PLAYLIST } from "../utils/mutations/playlistMutations"
-import { message } from "antd"
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import orange from "../assets/orange.png";
+import shakeygraves from "../assets/shakeygraves.jpg";
+import "./styles/PlaylistList.scss";
+import "./Dashboard/styles/Dashboard2.scss";
+import playBtn from "../assets/playBtn.png";
+import pauseBtn from "../assets/pauseBtn.png";
+import { DeleteOutlined } from "@ant-design/icons";
+import { useMutation } from "@apollo/client";
+import { REMOVE_FROM_PLAYLIST } from "../utils/mutations/playlistMutations";
+import { message } from "antd";
 const PlaylistList = ({
-    currentPlayer,
-    data,
-    setIsPlaying,
-    isPlaying,
-    currentSong,
-    getSongInfo,
-    setCurrentSong,
-    singlePL,
-    setDeleting,
-    refetch,
+  currentPlayer,
+  data,
+  setIsPlaying,
+  isPlaying,
+  currentSong,
+  getSongInfo,
+  setCurrentSong,
+  singlePL,
+  setDeleting,
+  refetch,
 }) => {
-    let navigate = useNavigate()
-    const [removeFromPlaylist, { error }] = useMutation(REMOVE_FROM_PLAYLIST)
-    const handleSongClick = (song) => {
-        navigate(`/song/${song._id}`)
+  let navigate = useNavigate();
+  const [removeFromPlaylist, { error }] = useMutation(REMOVE_FROM_PLAYLIST);
+  const handleSongClick = (song) => {
+    navigate(`/song/${song._id}`);
+  };
+  const token = localStorage.getItem("token");
+  const handleDelete = async (event, song) => {
+    setDeleting(true);
+    event.preventDefault();
+
+    try {
+      await removeFromPlaylist({
+        variables: {
+          playlistId: singlePL._id,
+          songId: song._id,
+          token: token,
+        },
+      });
+      await message.success("Removed song from playlist.");
+      await refetch();
+    } catch (err) {
+      message.error("Error removing song from playlist.");
     }
-    const token = localStorage.getItem("token")
-    const handleDelete = async (event, song) => {
-        setDeleting(true)
-        event.preventDefault()
+    setDeleting(false);
+  };
 
-        try {
-            await removeFromPlaylist({
-                variables: {
-                    playlistId: singlePL._id,
-                    songId: song._id,
-                    token: token,
-                },
-            })
-            await message.success("Successfully removed song from playlist.")
-            await refetch()
-        } catch (err) {
-            message.error("Error removing song from playlist.")
-        }
-        setDeleting(false)
-    }
-
-    return (
-        <div className="playlist-data-container">
-            <div className="playlist-information-header">
-                <img src={shakeygraves} alt="Shakey Graves" />
-                <div>
-                    <h3>{data.plTitle}</h3>
-                    <h4>
-                        {data.songs.length === 1
-                            ? `${data.songs.length} song`
-                            : `${data.songs.length} songs`}
-                    </h4>
-                </div>
-            </div>
-
-            {data.songs.map((song) => {
-                return (
-                    <div className="playlist-song-row">
-                        <div className="playlist-song-information">
-                            <input
-                                onClick={(event) => {
-                                    event.preventDefault()
-                                    setIsPlaying(!isPlaying)
-                                    if (currentSong !== song.link) {
-                                        setIsPlaying(true)
-                                        setCurrentSong(song.link)
-                                    }
-                                    getSongInfo({
-                                        title: song.title,
-                                        artist: song.artist,
-                                    })
-
-                                    if (!isPlaying)
-                                        currentPlayer.current.pause()
-                                    else currentPlayer.current.play()
-                                }}
-                                style={{}}
-                                type="image"
-                                src={isPlaying ? pauseBtn : playBtn}
-                                name="playBtn"
-                                className="play-button"
-                                alt="play button"
-                            />
-                            <img
-                                src={song.cover ? song.cover : orange}
-                                alt="Album Cover"
-                                style={{ marginLeft: "1rem" }}
-                            />
-                            <div
-                                className="playlist-song-text"
-                                onClick={() => handleSongClick(song)}
-                            >
-                                <h4>{song.title}</h4>
-                                <h5>{song.artist}</h5>
-                            </div>
-                        </div>
-                        <div>
-                            <DeleteOutlined
-                                onClick={(event) => handleDelete(event, song)}
-                                style={{ fontSize: "1.2rem" }}
-                            />
-                        </div>
-                    </div>
-                )
-            })}
+  return (
+    <div className="playlist-data-container">
+      <div className="playlist-information-header">
+        <img src={shakeygraves} alt="Shakey Graves" />
+        <div>
+          <h3>{data.plTitle}</h3>
+          <h4>
+            {data.songs.length === 1
+              ? `${data.songs.length} song`
+              : `${data.songs.length} songs`}
+          </h4>
         </div>
-    )
-}
+      </div>
 
-export default PlaylistList
+      {data.songs.map((song) => {
+        return (
+          <div className="playlist-song-row">
+            <div className="playlist-song-information">
+              <input
+                onClick={(event) => {
+                  event.preventDefault();
+                  setIsPlaying(!isPlaying);
+                  if (currentSong !== song.link) {
+                    setIsPlaying(true);
+                    setCurrentSong(song.link);
+                  }
+                  getSongInfo({
+                    title: song.title,
+                    artist: song.artist,
+                  });
+
+                  if (!isPlaying) currentPlayer.current.pause();
+                  else currentPlayer.current.play();
+                }}
+                style={{}}
+                type="image"
+                src={isPlaying ? pauseBtn : playBtn}
+                name="playBtn"
+                className="play-button"
+                alt="play button"
+              />
+              <img
+                src={song.cover ? song.cover : orange}
+                alt="Album Cover"
+                style={{ marginLeft: "1rem" }}
+              />
+              <div
+                className="playlist-song-text"
+                onClick={() => handleSongClick(song)}
+              >
+                <h4>{song.title}</h4>
+                <h5>{song.artist}</h5>
+              </div>
+            </div>
+            <div>
+              <DeleteOutlined
+                onClick={(event) => handleDelete(event, song)}
+                style={{ fontSize: "1.2rem" }}
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default PlaylistList;
--- client/src/pages/Playlists.js
+++ client/src/pages/Playlists.js
@@ -1,183 +1,194 @@
-import { useState, useEffect } from "react"
-import { DeleteOutlined } from "@ant-design/icons"
-import "./styles/Playlists.css"
-import "../components/styles/PlaylistList.css"
-import "../components/Dashboard/styles/Dashboard2.scss"
-import { Empty, message } from "antd"
-import { useQuery } from "@apollo/client"
-import { GET_USER_PLAYLIST } from "../utils/queries/songQueries"
-import { useLocation } from "react-router-dom"
-import PlaylistList from "../components/PlaylistLists"
-import { Box, CircularProgress } from "@mui/material"
-import playlistIcon from "../assets/playlist.png"
-import { useMutation } from "@apollo/client"
-import { REMOVE_PLAYLIST } from "../utils/mutations/playlistMutations"
+import { useState, useEffect } from "react";
+import { DeleteOutlined } from "@ant-design/icons";
+import "./styles/Playlists.scss";
+import "../components/styles/PlaylistList.scss";
+import "../components/Dashboard/styles/Dashboard2.scss";
+import { Empty, message } from "antd";
+import { useQuery } from "@apollo/client";
+import { GET_USER_PLAYLIST } from "../utils/queries/songQueries";
+import { useLocation } from "react-router-dom";
+import PlaylistList from "../components/PlaylistLists";
+import { Box, CircularProgress } from "@mui/material";
+import playlistIcon from "../assets/playlist.png";
+import { useMutation } from "@apollo/client";
+import { REMOVE_PLAYLIST } from "../utils/mutations/playlistMutations";
 
 const Playlists = ({
-    currentPlayer,
-    singlePL,
-    setSinglePL,
-    setIsPlaying,
-    isPlaying,
-    currentSong,
-    getSongInfo,
-    setCurrentSong,
+  currentPlayer,
+  singlePL,
+  setSinglePL,
+  setIsPlaying,
+  isPlaying,
+  currentSong,
+  getSongInfo,
+  setCurrentSong,
 }) => {
-    const [removePlaylist, { error }] = useMutation(REMOVE_PLAYLIST)
-    const token = localStorage.getItem("token")
-    const [currentTarget, setCurrentTarget] = useState(null)
-    const username = localStorage.getItem("username")
-    const [playlistClicked, setPlaylistClicked] = useState(false)
-    const [title, setPlTitle] = useState("")
-    const [playlistSong, setPlaylistSong] = useState()
-    const [selectedSong, setSelectedSong] = useState(false)
-    const [newTitle, setTitle] = useState()
-    const [activeState, setActiveState] = useState(-1)
-    const [deleting, setDeleting] = useState(false)
-    const [r, setR] = useState(false)
-    const location = useLocation()
+  const [removePlaylist, { error }] = useMutation(REMOVE_PLAYLIST);
+  const token = localStorage.getItem("token");
+  const [currentTarget, setCurrentTarget] = useState(null);
+  const username = localStorage.getItem("username");
+  const [playlistClicked, setPlaylistClicked] = useState(false);
+  const [title, setPlTitle] = useState("");
+  const [playlistSong, setPlaylistSong] = useState();
+  const [selectedSong, setSelectedSong] = useState(false);
+  const [newTitle, setTitle] = useState();
+  const [activeState, setActiveState] = useState(-1);
+  const [deleting, setDeleting] = useState(false);
+  const [r, setR] = useState(false);
+  const location = useLocation();
 
-    const {
-        loading: playlistloading,
-        data,
-        refetch,
-    } = useQuery(GET_USER_PLAYLIST, {
-        variables: { owner: username },
-    })
-    let currentPlaylists = data?.userPlaylists || []
+  const {
+    loading: playlistloading,
+    data,
+    refetch,
+  } = useQuery(GET_USER_PLAYLIST, {
+    variables: { owner: username },
+  });
+  let currentPlaylists = data?.userPlaylists || [];
 
-    useEffect(() => {
-        if (data !== undefined)
-            localStorage.setItem("playlists", JSON.stringify(currentPlaylists))
-    }, [currentPlaylists])
+  useEffect(() => {
+    if (data !== undefined)
+      localStorage.setItem("playlists", JSON.stringify(currentPlaylists));
+  }, [currentPlaylists]);
 
-    useEffect(() => {
-        return setPlaylistClicked(false) && setSinglePL([])
-    }, [])
-    useEffect(() => {
-        const fetchPlaylists = async () => {
-            await refetch()
-        }
-        fetchPlaylists()
-    }, [location.pathname])
+  useEffect(() => {
+    return setPlaylistClicked(false) && setSinglePL([]);
+  }, []);
+  useEffect(() => {
+    const fetchPlaylists = async () => {
+      await refetch();
+    };
+    fetchPlaylists();
+  }, [location.pathname]);
 
-    useEffect(() => {
-        localStorage.setItem("singlePL", JSON.stringify(singlePL))
-    }, [singlePL])
+  useEffect(() => {
+    localStorage.setItem("singlePL", JSON.stringify(singlePL));
+  }, [singlePL]);
 
-    const switchPlaylist = (e) => {
-        e.preventDefault()
-        setPlTitle(e.currentTarget.outerText)
-        setPlaylistClicked(true)
+  const switchPlaylist = (e) => {
+    e.preventDefault();
+    setPlTitle(e.currentTarget.outerText);
+    setPlaylistClicked(true);
 
-        for (let i = 0; i < currentPlaylists.length; i++) {
-            if (currentPlaylists[i]._id === e.currentTarget.id) {
-                setCurrentTarget(e.currentTarget.id)
-                setSinglePL(currentPlaylists[i])
-            }
-        }
+    for (let i = 0; i < currentPlaylists.length; i++) {
+      if (currentPlaylists[i]._id === e.currentTarget.id) {
+        setCurrentTarget(e.currentTarget.id);
+        setSinglePL(currentPlaylists[i]);
+      }
     }
-    useEffect(() => {
-        if (currentTarget)
-            for (let each of currentPlaylists)
-                if (each._id === currentTarget) setSinglePL(each)
-    }, [currentPlaylists])
+  };
+  useEffect(() => {
+    if (currentTarget)
+      for (let each of currentPlaylists)
+        if (each._id === currentTarget) setSinglePL(each);
+  }, [currentPlaylists]);
 
-    const handleDelete = async (event, playlist) => {
-        setDeleting(true)
-        event.preventDefault()
-        try {
-            await removePlaylist({
-                variables: {
-                    playlistId: playlist._id,
-                    token: token,
-                },
-            })
-            await message.success("Removing playlist.")
-            await refetch()
-            await message.success("Playlist removed.")
-        } catch (err) {
-            console.log(err)
-            message.error("Error removing playlist.")
-        }
-        setDeleting(false)
+  const handleDelete = async (event, playlist) => {
+    setDeleting(true);
+    event.preventDefault();
+    try {
+      await removePlaylist({
+        variables: {
+          playlistId: playlist._id,
+          token: token,
+        },
+      });
+      await message.loading("Removing playlist.");
+      await refetch();
+      await message.success("Playlist removed.");
+    } catch (err) {
+      console.log(err);
+      message.error("Error removing playlist.");
     }
-    return playlistloading ? (
-        <div>
-            {" "}
-            <Box
-                sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    marginTop: "14rem",
-                }}
+    setDeleting(false);
+  };
+
+  return playlistloading ? (
+    <div>
+      {" "}
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "14rem",
+        }}
+      >
+        <CircularProgress style={{ color: "orange" }} />
+      </Box>
+    </div>
+  ) : (
+    <div style={{ display: "grid", gridTemplateColumns: "1fr 4fr" }}>
+      <div className="playlist-name-container">
+        <h2>Playlists:</h2>
+        {currentPlaylists.length === 0 && (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "2rem 0",
+            }}
+          >
+            <Empty description="No playlists created" />
+          </div>
+        )}
+        {currentPlaylists.map((playlist, index) => {
+          return (
+            <div
+              className={`playlist-button ${
+                index === activeState ? "active" : null
+              }`}
+              key={playlist.plTitle}
+              id={playlist._id}
             >
-                <CircularProgress style={{ color: "orange" }} />
-            </Box>
-        </div>
-    ) : (
-        <div style={{ display: "grid", gridTemplateColumns: "1fr 4fr" }}>
-            <div className="playlist-name-container">
-                <h2>Playlists:</h2>
-
-                {currentPlaylists.map((playlist, index) => {
-                    return (
-                        <div
-                            style={{ justifyContent: "space-between" }}
-                            key={playlist.plTitle}
-                            id={playlist._id}
-                            className={`playlist-button ${
-                                index === activeState ? "active" : null
-                            }`}
-                        >
-                            <img src={playlistIcon} alt="Playlist" />
-                            <div
-                                id={playlist._id}
-                                className="play-title"
-                                onClick={(e) => {
-                                    switchPlaylist(e)
-                                    setActiveState(index)
-                                }}
-                            >
-                                {playlist.plTitle}
-                            </div>
-                            <div>
-                                <DeleteOutlined
-                                    onClick={(event) =>
-                                        handleDelete(event, playlist)
-                                    }
-                                    style={{ fontSize: "1.2rem" }}
-                                />
-                            </div>
-                        </div>
-                    )
-                })}
+              <div
+                id={playlist._id}
+                className="play-title"
+                onClick={(e) => {
+                  switchPlaylist(e);
+                  setActiveState(index);
+                }}
+              >
+                <img src={playlistIcon} alt="Playlist" />
+                {playlist.plTitle}
+              </div>
+              <div>
+                <DeleteOutlined
+                  onClick={(event) => handleDelete(event, playlist)}
+                  style={{
+                    fontSize: "1.2rem",
+                    padding: ".5rem 0",
+                  }}
+                />
+              </div>
             </div>
-            <div className="playlist-container">
-                <div className="content">
-                    {playlistClicked ? (
-                        <PlaylistList
-                            refetch={refetch}
-                            setDeleting={setDeleting}
-                            singlePL={singlePL}
-                            currentPlayer={currentPlayer}
-                            data={singlePL}
-                            setIsPlaying={setIsPlaying}
-                            isPlaying={isPlaying}
-                            currentSong={currentSong}
-                            getSongInfo={getSongInfo}
-                            setCurrentSong={setCurrentSong}
-                        />
-                    ) : (
-                        <div className="no-playlist-selected">
-                            <Empty description="No playlist selected" />
-                        </div>
-                    )}
-                </div>
+          );
+        })}
+      </div>
+      <div className="playlist-container">
+        <div className="content">
+          {playlistClicked ? (
+            <PlaylistList
+              refetch={refetch}
+              setDeleting={setDeleting}
+              singlePL={singlePL}
+              currentPlayer={currentPlayer}
+              data={singlePL}
+              setIsPlaying={setIsPlaying}
+              isPlaying={isPlaying}
+              currentSong={currentSong}
+              getSongInfo={getSongInfo}
+              setCurrentSong={setCurrentSong}
+            />
+          ) : (
+            <div className="no-playlist-selected">
+              <Empty description="No playlist selected" />
             </div>
+          )}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Playlists
+export default Playlists;
