bf3f9cf9beee06fc5c425548b3334dda88991a48
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-06-29 23:26:19 -0700

    merge fix

--- client/src/components/Dashboard/DashResultCard.js
+++ client/src/components/Dashboard/DashResultCard.js
@@ -5,63 +5,41 @@ import "./styles/DashResultCard.css"
 import { useNavigate } from "react-router-dom"
 import orange from "../../assets/orange.png"
 const DashResultCard = ({
-  dashSearchResults,
-  setOneSongClick,
-  currentPlayer,
-  setCurrentSong,
-  setIsPlaying,
-  getSongInfo,
-  setSinglePL,
+    dashSearchResults,
+    setOneSongClick,
+    currentPlayer,
+    setCurrentSong,
+    setIsPlaying,
+    getSongInfo,
+    setSinglePL,
 }) => {
-  let searchResults = dashSearchResults;
-  let navigate = useNavigate();
+    let searchResults = dashSearchResults
+    let navigate = useNavigate()
 
-  const handleSearchClick = (event) => {
-    event.preventDefault();
-    getSongInfo(dashSearchResults);
-    setIsPlaying(true);
-    if (setOneSongClick !== undefined) {
-      setOneSongClick(true);
-      currentPlayer.current.src = event.currentTarget.attributes.name.value;
-      setCurrentSong(event.currentTarget.attributes.name.value);
-      setSinglePL([]);
+    const handleSearchClick = (event) => {
+        event.preventDefault()
+        getSongInfo(dashSearchResults)
+        setIsPlaying(true)
+        if (setOneSongClick !== undefined) {
+            setOneSongClick(true)
+            currentPlayer.current.src =
+                event.currentTarget.attributes.name.value
+            setCurrentSong(event.currentTarget.attributes.name.value)
+            setSinglePL([])
+        }
     }
-  };
 
-  const handleSongClick = (searchResults) => {
-    navigate(`/song/${searchResults._id}`);
-  };
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        flexWrap: "wrap",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          flexWrap: "wrap",
-          width: "100%",
-        }}
-      >
+    const handleSongClick = (searchResults) => {
+        navigate(`/song/${searchResults._id}`)
+    }
+    return (
         <div
-          className="dash-result-card-container"
-          onClick={() => handleSongClick(searchResults)}
-        >
-          <div
             style={{
-              zIndex: 1000,
-              display: "flex",
-              alignItems: "center",
-              width: "50rem",
-              margin: "0 auto",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                flexWrap: "wrap",
             }}
-<<<<<<< HEAD
         >
             <div
                 style={{
@@ -85,29 +63,63 @@ const DashResultCard = ({
                             margin: "0 auto",
                         }}
                     >
-                        <img
-                            src={
-                                searchResults.cover
-                                    ? searchResults.cover
-                                    : orange
-                            }
-                            alt="Album Cover"
-                            className="dash-result-img"
-                        />
                         <div
                             style={{
-                                marginLeft: "2rem",
+                                display: "flex",
+                                flexDirection: "row",
+                                justifyContent: "center",
+                                flexWrap: "wrap",
+                                width: "100%",
                             }}
                         >
-                            <h4 className="song-title">
-                                {searchResults.title}
-                            </h4>
-                            <h5 className="song-artist">
-                                {searchResults.artist}
-                            </h5>
-                            <h6 className="song-genre">
-                                {searchResults.genre}
-                            </h6>
+                            <div
+                                className="dash-result-card-container"
+                                onClick={() => handleSongClick(searchResults)}
+                            >
+                                <div
+                                    style={{
+                                        zIndex: 1000,
+                                        display: "flex",
+                                        alignItems: "center",
+                                        width: "50rem",
+                                        margin: "0 auto",
+                                    }}
+                                >
+                                    <img
+                                        src={
+                                            searchResults.cover
+                                                ? searchResults.cover
+                                                : orange
+                                        }
+                                        alt="Album Cover"
+                                        className="dash-result-img"
+                                    />
+                                    <div
+                                        style={{
+                                            marginLeft: "2rem",
+                                        }}
+                                    >
+                                        <h4 className="song-title">
+                                            {searchResults.title}
+                                        </h4>
+                                        <h5 className="song-artist">
+                                            {searchResults.artist}
+                                        </h5>
+                                        <h6 className="song-genre">
+                                            {searchResults.genre}
+                                        </h6>
+                                    </div>
+                                </div>
+                                <div
+                                    style={{
+                                        marginTop: -50,
+                                        position: "relative",
+                                        display: "flex",
+                                        flexWrap: "wrap",
+                                        justifyContent: "center",
+                                    }}
+                                ></div>
+                            </div>
                         </div>
                     </div>
                     <div
@@ -120,46 +132,9 @@ const DashResultCard = ({
                         }}
                     ></div>
                 </div>
-=======
-          >
-            <img
-              src={searchResults.cover ? searchResults.cover : orange}
-              alt="Album Cover"
-              className="dash-result-img"
-            />
-            {/* <PlayCircleTwoTone
-              className="result-play-btn"
-              name={searchResults.link}
-              onClick={handleSearchClick}
-              twoToneColor="#FFA500"
-              style={{
-                fontSize: "3rem",
-              }}
-            /> */}
-            <div
-              style={{
-                marginLeft: "2rem",
-              }}
-            >
-              <h4 className="song-title">{searchResults.title}</h4>
-              <h5 className="song-artist">{searchResults.artist}</h5>
-              <h6 className="song-genre">{searchResults.genre}</h6>
->>>>>>> a6ba8252a97a1a80a8565516bff0cccd750e86d7
             </div>
-          </div>
-          <div
-            style={{
-              marginTop: -50,
-              position: "relative",
-              display: "flex",
-              flexWrap: "wrap",
-              justifyContent: "center",
-            }}
-          ></div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default DashResultCard;
+export default DashResultCard
